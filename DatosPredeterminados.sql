53bcd838b63581287fbb5a53a4d51c01d95db624
MigueGalma <miguelignaciogalmarini@outlook.com>
2023-12-09 00:21:49 -0300

    subida de datos

---
+++ DatosPredeterminados.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: 
+-- Date: 2023-12-09 00:19
+*/
+INSERT INTO `` (`idCodigos`,`tamano`,`orientacion`,`precio`) VALUES (1090060,'90 cm x 60 cm','Individual',6000);
+INSERT INTO `` (`idCodigos`,`tamano`,`orientacion`,`precio`) VALUES (2075070,'75 cm x 70 cm','Horizontal',9000);
+INSERT INTO `` (`idCodigos`,`tamano`,`orientacion`,`precio`) VALUES (2100070,'100 cm x 70 cm','Horizontal',12000);
+INSERT INTO `` (`idCodigos`,`tamano`,`orientacion`,`precio`) VALUES (2150070,'150 cm x 70 cm','Horizontal',15000);
+INSERT INTO `` (`idCodigos`,`tamano`,`orientacion`,`precio`) VALUES (3075070,'75 cm x 70 cm','Vertical',9000);
+INSERT INTO `` (`idCodigos`,`tamano`,`orientacion`,`precio`) VALUES (3100070,'100 cm x 70 cm','Vertical',12000);
+INSERT INTO `` (`idCodigos`,`tamano`,`orientacion`,`precio`) VALUES (3150070,'150 cm x 70 cm','Vertical',15000);
